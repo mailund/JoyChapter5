b0875fd3ebcf18c991b9ea3c941e53732a7d6b32
Thomas Mailund <tm@kvantify.dk>
2024-04-27 15:03:51 +0200

    Moved code generation to header file

--- static_linked_list.h
+++ static_linked_list.h
@@ -59,4 +59,68 @@ generic_push_new_link(struct generic_link **list, size_t link_size);
 void
 delete_link(struct list_iter itr);
 
+#define GEN_STRUCTS(LIST_NAME, KEY_TYPE)                                       \
+  struct LIST_NAME##_link {                                                    \
+    LINK_HEADER;                                                               \
+    KEY_TYPE key;                                                              \
+  };                                                                           \
+  struct LIST_NAME##_list {                                                    \
+    LIST_HEADER;                                                               \
+  };
+
+#define GEN_ADD_KEY(LIST_NAME, KEY_TYPE)                                       \
+  void LIST_NAME##_add_key(struct LIST_NAME##_list *list, KEY_TYPE key)        \
+  {                                                                            \
+    push_new_link(list, sizeof(struct LIST_NAME##_link));                      \
+    struct LIST_NAME##_link *link = void_cast(list_begin(list));               \
+    link->key = key;                                                           \
+  }
+
+#define GEN_FREE_LIST(LIST_NAME, KEY_TYPE, FREE_KEY)                           \
+  void LIST_NAME##_free_list(struct LIST_NAME##_list *list)                    \
+  {                                                                            \
+    for (struct list_iter itr = list_begin(list); !list_end(itr);              \
+         itr = list_next(itr)) {                                               \
+      FREE_KEY(cast_itr(struct LIST_NAME##_link, itr)->key);                   \
+    }                                                                          \
+    free_linked_list(list);                                                    \
+    list->generic = NULL;                                                      \
+  }
+
+#define GEN_DELETE_KEY(LIST_NAME, KEY_TYPE, IS_EQ, FREE_KEY)                   \
+  void LIST_NAME##_delete_key(struct LIST_NAME##_list *list,                   \
+                              const KEY_TYPE key)                              \
+  {                                                                            \
+    for (struct list_iter itr = list_begin(list); !list_end(itr);              \
+         itr = list_next(itr)) {                                               \
+      struct LIST_NAME##_link *link = void_cast(itr);                          \
+      if (IS_EQ(link->key, key)) {                                             \
+        FREE_KEY(link->key);                                                   \
+        delete_link(itr);                                                      \
+        return;                                                                \
+      }                                                                        \
+    }                                                                          \
+  }
+
+#define GEN_CONTAINS_KEY(LIST_NAME, KEY_TYPE, IS_EQ)                           \
+  bool LIST_NAME##_contains_key(struct LIST_NAME##_list *list,                 \
+                                const KEY_TYPE key)                            \
+  {                                                                            \
+    for (struct list_iter itr = list_begin(list); !list_end(itr);              \
+         itr = list_next(itr)) {                                               \
+      struct LIST_NAME##_link *link = void_cast(itr);                          \
+      if (IS_EQ(link->key, key)) {                                             \
+        return true;                                                           \
+      }                                                                        \
+    }                                                                          \
+    return false;                                                              \
+  }
+
+#define GEN_LIST(LIST_NAME, KEY_TYPE, IS_EQ, FREE_KEY)                         \
+  GEN_STRUCTS(LIST_NAME, KEY_TYPE);                                            \
+  GEN_ADD_KEY(LIST_NAME, KEY_TYPE);                                            \
+  GEN_DELETE_KEY(LIST_NAME, KEY_TYPE, IS_EQ, FREE_KEY);                        \
+  GEN_CONTAINS_KEY(LIST_NAME, KEY_TYPE, IS_EQ);                                \
+  GEN_FREE_LIST(LIST_NAME, KEY_TYPE, FREE_KEY);
+
 #endif
